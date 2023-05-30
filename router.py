9ba0a5b53f5ccc647f9df03de30fe1f664d63084
jaishreegullani <jaishree.gullani@47billion.com>
2023-05-30 16:56:15 +0530

    adding json api

--- router.py
+++ router.py
@@ -1,6 +1,7 @@
 from celery import chain
 from fastapi import FastAPI, HTTPException, status
-from tasks import add_metadata, open_webpage
+
+from tasks import add_metadata, open_webpage, get_json_from_db
 
 app = FastAPI()
 
@@ -8,7 +9,13 @@ app = FastAPI()
 @app.post("/metadata", status_code=status.HTTP_200_OK)
 def get_data(data: dict):
     try:
-        res = chain(add_metadata.s(data), open_webpage.s()).apply_async()
+        # res = chain(add_metadata.s(data), open_webpage.s()).apply_async()
+        res = chain(add_metadata.s(data)).apply_async()
         return {'data updated': res.parent.get()}
     except HTTPException:
         HTTPException(status_code=501, detail='error in updating metadata on postgres')
+
+
+@app.get("/json/{request_id}")
+def get_json(request_id: int):
+    return get_json_from_db(request_id)
--- tasks.py
+++ tasks.py
@@ -1,9 +1,12 @@
 from datetime import datetime
 
 from fastapi import HTTPException
+from minio.error import S3Error
 from playwright.sync_api import sync_playwright
 
+import env
 from Connections.celery_connection import celery_app
+from Connections.minio_connection import minio_client
 from Connections.postgres_connection import session
 from schemas import video_metadata
 
@@ -43,3 +46,37 @@ def open_webpage(request_id):
         # video_url = video.get_property('src')
         # urllib.request.urlretrieve(str(video_url), 'videoname.mp4')
         browser.close()
+
+
+# @celery_app.task(queue='minio')
+# def add_video(req_id):
+#     my_bucket = env.MINIO_BUCKET
+#     bucket = minio_client.bucket_exists(my_bucket)
+#     if not bucket:
+#         minio_client.make_bucket(my_bucket)
+#     try:
+#         minio_client.fput_object(
+#             bucket_name=my_bucket,
+#             object_name=f'{req_id}/video.mp4',
+#             file_path='videos/Welcome.mp4'
+#         )
+#         return req_id
+#     except S3Error as e:
+#         print('ERROR IN MINIO', e)
+#         HTTPException(status_code=501, detail='error in updating video file in minio')
+
+
+# @celery_app.task()
+# def update_database(req_id):
+#     print("Inside update_database")
+#     session.query(video_metadata).filter(video_metadata.id == req_id).update()
+#     session.commit()
+
+
+@celery_app.task()
+def get_json_from_db(req_id: int):
+    try:
+        result = session.query(video_metadata).get(req_id)
+        return result
+    except Exception as e:
+        HTTPException(status_code=501, detail='error in fetching details from database')
