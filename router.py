8247d720ad40e6e7654427f64e0670737bd2dd2c
jaishreegullani <jaishree.gullani@47billion.com>
2023-05-31 14:23:20 +0530

    adding changes

--- router.py
+++ router.py
@@ -12,14 +12,17 @@ def get_data(data: dict, background_tasks: BackgroundTasks):
         background_tasks.add_task(open_webpage, req_id)
         return {'request_id': req_id}
     except HTTPException:
-        raise HTTPException(status_code=501, detail='error in updating metadata on database')
+        raise HTTPException(status_code=502, detail=f'error in updating metadata on database {e}')
 
 
 @app.get("/json/{request_id}")
 def get_json(request_id: int):
-    return get_json_from_db(request_id)
+    try:
+        return get_json_from_db(request_id)
+    except Exception:
+        raise HTTPException(status_code=502, detail='error in fetching json from database')
 
 
-@app.post("/video")
-def save_video():
+@app.post("/video/{request_id}")
+def save_video(request_id):
     pass
--- service.py
+++ service.py
@@ -1,4 +1,5 @@
 from datetime import datetime
+from time import sleep
 
 from fastapi import HTTPException
 from playwright.sync_api import sync_playwright
@@ -10,17 +11,17 @@ from schemas import video_metadata
 
 def add_metadata(data: dict):
     try:
-        metadata_object = video_metadata(metadata_info=data, uploaded_time=datetime.now())
+        metadata_object = video_metadata(id=5, metadata_info=data, uploaded_time=datetime.now())
         session.add(metadata_object)
         session.commit()
         req_id = metadata_object.id
         logger.info('added metadata info in database')
         return req_id
 
-    except ConnectionError:
+    except Exception as e:
         session.rollback()
         logger.error('error in adding metadata to database')
-        raise HTTPException(status_code=501, detail='error in updating metadata on postgres')
+        raise HTTPException(status_code=502, detail=f'error in updating metadata on database ERROR: {e}')
 
 
 def open_webpage(request_id):
@@ -29,17 +30,12 @@ def open_webpage(request_id):
         browser = p.firefox.launch(headless=False)
         page = browser.new_page()
         page.goto(f'http://localhost:3000/render/{request_id}')
-
         page.wait_for_selector('button')
-
-        # Click the play button
         page.click('button')
-
-        # Wait for the video to load
         # video_element = page.wait_for_selector('video')
-        while not page.evaluate(' document.querySelector("video").ended'):
-            continue
-
+        # while not page.evaluate(' document.querySelector("video").ended'):
+        #     continue
+        sleep(10)
         browser.close()
 
 
@@ -47,5 +43,6 @@ def get_json_from_db(req_id):
     try:
         result = session.query(video_metadata).get(req_id)
         return result.metadata_info
-    except ConnectionError as e:
-        raise HTTPException(status_code=501, detail=f'error in fetching json from database with ERROR: {e}')
+    except Exception as e:
+        logger.error(f'error in fetching json from database {e}')
+        raise HTTPException(status_code=502, detail='error in fetching json from database')
